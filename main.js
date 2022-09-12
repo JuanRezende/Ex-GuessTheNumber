266936bd55ff9045ce4711d3eb747e3ea586e7e9
Juan Rezende <37057092+JuanRezende@users.noreply.github.com>
2022-09-12 15:48:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+
+let randomNumber = Math.round(Math.random() * 10);
+let xAttempts = 1;
+
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
+document.addEventListener("keydown", handleEnterKey);
+
+function handleTryClick(event) {
+  event.preventDefault();
+
+  const inputNumber = document.querySelector("#inputNumber");
+
+  if (Number(inputNumber.value) === randomNumber) {
+    toggleScreen();
+
+    if (xAttempts == 1) {
+      screen2.querySelector(
+        "h2"
+      ).innerHTML = `Acertou em ${xAttempts} tentativa`;
+    } else {
+      screen2.querySelector(
+        "h2"
+      ).innerHTML = `Acertou em ${xAttempts} tentativas`;
+    }
+  }
+
+  inputNumber.value = "";
+
+  xAttempts++;
+}
+
+function handleResetClick() {
+  toggleScreen();
+
+  randomNumber = Math.round(Math.random() * 10);
+  xAttempts = 1;
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
+
+function handleEnterKey(e) {
+  if (e.key == "Enter" && (screen1.classList.contains('hide'))) {
+    handleResetClick();
+  }
+}
